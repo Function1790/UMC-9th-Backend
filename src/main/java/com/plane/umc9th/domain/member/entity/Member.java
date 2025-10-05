bcd7662b18baf10dc95b84a872baf26223bea065
Synt4xT3rr0r(Function) <67085909+Function1790@users.noreply.github.com>
2025-10-05 17:15:29 +0900

    feat#Chapter4: misson, member, restaurant, catergory

--- src/main/java/com/plane/umc9th/domain/member/entity/Member.java
+++ src/main/java/com/plane/umc9th/domain/member/entity/Member.java
@@ -1,7 +1,15 @@
 package com.plane.umc9th.domain.member.entity;
 
+import com.plane.umc9th.domain.catergory.entity.FoodCatergory;
+import com.plane.umc9th.domain.member.enums.Gender;
+import com.plane.umc9th.domain.member.enums.Provider;
+import com.plane.umc9th.domain.member.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Builder
@@ -14,10 +22,50 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Foreign Keys
+    @OneToOne
+    @JoinColumn(name = "settingId")
+    private MemberSetting setting;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<FoodCatergoryLikes> foodCatergoryLikes;
+
+    // Term
+
+    // Columns
     @Column
     private String name;
 
     @Column
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+
+    @Column
+    private String address;
+
+    @Column
+    private String email;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Column
+    private int point;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private boolean is_verified;
+
+    @Column(columnDefinition = "DATETIME")
+    private LocalDateTime inactive_date;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
